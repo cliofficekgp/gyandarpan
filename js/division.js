c6179ac505cd4f08c2f24a0a7dbf26f8a88738b4
AnirbanKar-web <68363841+AnirbanKar-web@users.noreply.github.com>
2024-11-12 21:23:16 +0530

    gyandarpan initial commit

---
+++ js/division.js
@@ -0,0 +1,4 @@
+var Division=[
+{"Division_Id":"1", "Division_Name":"SWR"},
+{"Division_Id":"2", "Division_Name":"Three Phase"},
+{"Division_Id":"3", "Division_Name":"Convensional"}];
---
+++ js/ochapter.js
@@ -0,0 +1,10 @@
+var OChapter=[
+{"Chapter_Id":"1", "Division_Id":"1", "Chapter_Name":"Chapter 1"},
+{"Chapter_Id":"2", "Division_Id":"1", "Chapter_Name":"Chapter 2"},
+{"Chapter_Id":"3", "Division_Id":"1", "Chapter_Name":"Chapter 3"},
+{"Chapter_Id":"1", "Division_Id":"2", "Chapter_Name":"Chapter 2 1"},
+{"Chapter_Id":"2", "Division_Id":"2", "Chapter_Name":"Chapter 2 2"},
+{"Chapter_Id":"3", "Division_Id":"2", "Chapter_Name":"Chapter 2 3"},
+{"Chapter_Id":"1", "Division_Id":"3", "Chapter_Name":"Chapter 3 1"},
+{"Chapter_Id":"2", "Division_Id":"3", "Chapter_Name":"Chapter 3 2"}
+];
